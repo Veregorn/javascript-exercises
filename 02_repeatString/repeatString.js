eab5c5f9601ce3655578aec3a1f9b8f0cbb778c9
Raul Jimenez <rjbarco@gmail.com>
2022-03-09 18:17:42 +0100

    Ejercicio resuelto

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let stResult = '';
+    while (num > 0) {
+        stResult += str;
+        num--;
+    }
+    if (num < 0) {
+        stResult = 'ERROR';
+    }
+    return stResult;
 };
 
 // Do not edit below this line
