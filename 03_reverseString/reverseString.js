c5ea5a341dbb5b363ceb2413278108b96d09b4b2
Raul Jimenez <rjbarco@gmail.com>
2022-03-10 10:34:24 +0100

    Ejercicio resuelto

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strResult = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        strResult += str[i];
+    }
+    return strResult;
 };
 
 // Do not edit below this line
