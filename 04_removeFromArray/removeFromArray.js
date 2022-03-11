9963f371b2d2d45ca365ef18bb1f7b454977ca67
Raul Jimenez <rjbarco@gmail.com>
2022-03-11 10:49:34 +0100

    Ejercicio resuelto

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (typeof arguments[j] === typeof numbers[i]) {
+                if (numbers[i] == arguments[j]) {
+                    numbers.splice(i,1);
+                    i--;
+                    break;
+                }
+            } 
+        }
+    }
+    return numbers;
 };
 
 // Do not edit below this line
