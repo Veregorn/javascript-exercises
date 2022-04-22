79d864b2daeebac9abbb411aa9c55b2a77b9d236
Raul Jimenez <rjbarco@gmail.com>
2022-04-22 13:17:00 +0200

    Exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Letters to lower case
+    str = str.toLowerCase();
+    // Delete spaces (all instances with /g modifier)
+    str = str.replace(/[ ]/g,'');
+    // Delete punctuation signs
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
+    let i = 0;
+    let j = str.length - 1;
+    let isPalindrome = true;
+    while (i < j) {
+        if (str[i] != str[j]) {
+            isPalindrome = false;
+            break;
+        }
+        i++;
+        j--;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
