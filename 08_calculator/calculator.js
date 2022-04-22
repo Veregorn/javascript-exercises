af6128aecbcb39d6c3df9fd1391c9824030bf296
Raul Jimenez <rjbarco@gmail.com>
2022-04-22 07:12:32 +0200

    Exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let result = 0;
+  numbers.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  numbers.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  while (number > 1) {
+    result *= number;
+    number--;
+  }
+  return result;
 };
 
 // Do not edit below this line
