6032e09eefd036ad7f357c3930a9cd4c54e3d717
Raul Jimenez <rjbarco@gmail.com>
2022-04-23 08:26:31 +0200

    Exercise solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldToYoung = people.sort((a,b) => {
+        const actualYear = new Date().getFullYear();
+        if ((a.yearOfDeath != undefined) && (b.yearOfDeath != undefined)) {
+            if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if ((a.yearOfDeath == undefined) && (b.yearOfDeath == undefined)) {
+            if ((actualYear - a.yearOfBirth) > (actualYear - b.yearOfBirth)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (a.yearOfDeath == undefined) {
+            if ((actualYear - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (b.yearOfDeath == undefined) {
+            if ((a.yearOfDeath - a.yearOfBirth) > (actualYear - b.yearOfBirth)) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        
+    });
+    return oldToYoung[0];
 };
 
 // Do not edit below this line
