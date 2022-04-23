2e51a91339e862cb48ce65725a4ac68e1e67eb8b
Raul Jimenez <rjbarco@gmail.com>
2022-04-23 07:18:16 +0200

    Exercise solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    //First I need to create a initial fibonacci array with 2 elements
+    let fibArray = [0,1];
+    //If index received is greater than 1 we need to calc more elements in fibonacci sequence
+    if (index > 1) {
+        for (let i = 2; i <= index; i++) {
+            fibArray.push(fibArray[i-2] + fibArray[i-1]);    
+        }
+        return fibArray[index];
+    } else if (index >= 0) {
+        return fibArray[index];
+    } else {
+        return "OOPS";
+    }
+    
 };
 
 // Do not edit below this line
